6fcf2c223c9258a317d03fb5b314905a1b651c53
Ubuntu <ubuntu@ip-172-31-36-89.ec2.internal>
2019-10-17 15:50:18 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    message_course: 'Inserta el nombre del curso',
+    message_credits: 'Inserta los creditos',
+    courseName: '',
+    credits: ''
+  },
+  methods: {
+    createREST: function () {
+     
+    },
+    createGraphQL: function () {
+     
+    }
+  }
+})
